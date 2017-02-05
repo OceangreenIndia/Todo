cb9df4b38672b371c2b2da7ca1eafae7fac16c7d
Rahul Shahare <rahulshahare@gmail.com>
2017-02-05 15:53:35 +0530

    url, random number, shorturl

--- php/RandomNumberLogic.php
+++ php/RandomNumberLogic.php
@@ -1,12 +1,15 @@
 <?php
 $input_url = mysql_real_escape_string($_POST['url']);
+
 if(empty($input_url) || (filter_var($input_url, FILTER_VALIDATE_URL) === false)){
 	header("location: #FileNameWhereYouWantTORedirectOnIfPostUrlIsEmptyOrItsInvalidUrl");
+	
   }else{
-  $id=rand(10000,99999);
-  $shorturl=base_convert($id,20,36);
-  echo "id = ". $id."</br>";
-  echo "short = ".$shorturl."<br/>";
+	  $id=rand(10000,99999);
+	  $shorturl=base_convert($id,20,36);
+	  echo "id = ". $id."</br>";
+	  echo "short = ".$shorturl."<br/>";
+	  echo 'Url is '.$input_url;
   }
   
   ?>
